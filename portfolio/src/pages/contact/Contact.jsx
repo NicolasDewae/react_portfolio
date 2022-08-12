d3ce4d55fe9924afe5316b2deee29d294cb8e61f
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-08-12 11:23:41 +0200

    contact page init

---
+++ portfolio/src/pages/contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../../components/footer/Footer';
+import './Contact.css';
+
+const Contact = () => {
+    return (
+        <>
+            <Footer/>
+        </>
+    );
+}
+
+export default Contact;
