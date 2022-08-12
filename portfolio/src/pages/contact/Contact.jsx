5489c7bb68c06958f98cf213a8b3508d3c251d77
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-08-12 16:15:07 +0200

    add navbar at the contact page

--- portfolio/src/pages/contact/Contact.jsx
+++ portfolio/src/pages/contact/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from '../../components/footer/Footer';
+import Navbar from '../../components/navbar/Navbar';
 import './Contact.css';
 
 const Contact = () => {
     return (
         <>
+            <Navbar/>
             <Footer/>
         </>
     );
