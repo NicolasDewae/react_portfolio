cd775b809dfc9f70eb7389cc182f14938f90129b
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-08-12 16:14:02 +0200

    route added

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -12,14 +12,14 @@ const Navbar = () => {
         <>
             <div className="header">
                 <div className="title">
-                    <a>Nicolas De Waegenaere</a>
+                    <a href="/">Nicolas De Waegenaere</a>
                 </div>
                 <nav className={`nav ${showLinks ? "showNav" : "hideNav"}`}>
                     <ul className="links">
-                        <li><a href="#">Accueil</a></li>
-                        <li><a href="#">A propos</a></li>
-                        <li><a href="#">Projets</a></li>
-                        <li><a href="#">Contact</a></li>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/projects">Projects</a></li>
+                        <li><a href="/contact">Contact</a></li>
                     </ul>
                     <button className="burger" onClick={handleShowLinks}>
                         <span className="burger_line"></span>
