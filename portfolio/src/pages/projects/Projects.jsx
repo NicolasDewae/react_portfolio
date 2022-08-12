dc4c8a29b885c5d4681e7b10218ebbbcc4ce8b87
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-08-12 16:14:42 +0200

    projects component created

---
+++ portfolio/src/pages/projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../../components/footer/Footer';
+import Navbar from '../../components/navbar/Navbar';
+import './Projects.css';
+
+const Project = () => {
+    return (
+        <>
+            <Navbar/>
+            <Footer/>
+        </>
+    );
+}
+
+export default Project;
