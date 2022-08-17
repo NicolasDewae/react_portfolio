b93da7f0af4266d80027acb4b114942cea06ab17
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2022-08-17 16:21:04 +0200

    title en split component added

--- portfolio/src/pages/about/About.jsx
+++ portfolio/src/pages/about/About.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import Footer from '../../components/footer/Footer';
 import Navbar from '../../components/navbar/Navbar';
 import './About.css';
+import Split from '../../components/split/Split';
 
 const About = () => {
     return (
         <>
-            <Navbar/>
+            <Navbar />
+            <div>
+                <h1>About</h1>
+            </div>
+            <Split />
             <Footer/>
         </>
     );
