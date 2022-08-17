295932729f56c3712449541488b4d0ce8fcb79b7
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2022-08-17 16:19:59 +0200

    preview articles

---
+++ portfolio/src/components/previewArticle/PreviewArticle.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './PreviewArticle.css';
+import { Link } from 'react-router-dom';
+import Split from '../split/Split';
+
+const PreviewArticle = ({
+    
+    articles: {
+        pathway,
+        title,
+        picture,
+    }
+
+}) => {
+    
+    return (
+        <>
+            <div className="preview">
+                <div className="picture">
+                    <img src={picture} alt="photo de l'article" />
+                </div>
+                <div className="title">
+                    <h2>{title}</h2>
+                </div>
+                <Link to={`/blog/${pathway}`}>
+                    <a className="button">
+                        Voir le projet
+                    </a>
+                </Link>
+            </div>
+            <div>
+                <Split />
+            </div>
+        </>
+    );
+}
+
+export default PreviewArticle;
