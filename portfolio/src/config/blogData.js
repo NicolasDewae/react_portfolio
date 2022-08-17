2611e55358eaed41349f227c2c5b59e57ca99d02
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2022-08-17 16:21:22 +0200

    datas

---
+++ portfolio/src/config/blogData.js
@@ -0,0 +1,32 @@
+const blogData = [
+    {
+      id: 1,
+      pathway: 'titrePremierArticle',
+      title: 'Titre du premier article',
+      picture: '/assets/img/street_view-2.jpg',
+      description: 'Longue description du premier article, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisi vel consectetur interdum, nisl nunc consectetur nisi, eget consectetur nisi nisl eget consectetur nisi.',
+      blogImages: [
+          '/assets/img/street_view-2.jpg',
+          '/assets/img/street_view-2.jpg',
+          '/assets/img/street_view-2.jpg',
+          '/assets/img/street_view-2.jpg',
+          '/assets/img/street_view-2.jpg',
+      ]    
+    },
+    {
+        id: 2,
+        pathway: 'titreDeuxiemeArticle',
+        title: 'Titre du second article',
+        picture: '/assets/img/street_view-2.jpg',
+        description: 'Longue description du second article, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisi vel consectetur interdum, nisl nunc consectetur nisi, eget consectetur nisi nisl eget consectetur nisi.',
+        blogImages: [
+            '/assets/img/street_view-2.jpg',
+            '/assets/img/street_view-2.jpg',
+            '/assets/img/street_view-2.jpg',
+            '/assets/img/street_view-2.jpg',
+            '/assets/img/street_view-2.jpg',
+        ]
+    }
+]
+
+export default blogData;
---
+++ portfolio/src/config/projectsData.js
@@ -0,0 +1,60 @@
+const projectData = [
+    {
+      id: 1,
+      pathway: 'titrePremierProjet',
+      title: 'Titre du premier projet',
+      picture: '/assets/imgTemplate/1000x750.png',
+      description: 'Longue description du premier projet, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisi vel consectetur interdum, nisl nunc consectetur nisi, eget consectetur nisi nisl eget consectetur nisi.',
+      projectImages: [
+          '/assets/imgTemplate/1000x750.png',
+          '/assets/imgTemplate/1000x750.png',
+          '/assets/imgTemplate/1000x750.png',
+          '/assets/imgTemplate/1000x750.png',
+          '/assets/imgTemplate/1000x750.png',
+      ]    
+    },
+    {
+        id: 2,
+        pathway: 'titreDeuxiemeProjet',
+        title: 'Titre du second projet',
+        picture: '/assets/imgTemplate/1000x750.png',
+        description: 'Longue description du second projet, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisi vel consectetur interdum, nisl nunc consectetur nisi, eget consectetur nisi nisl eget consectetur nisi.',
+        projectImages: [
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/1000x750.png',
+        ]
+    },
+    {
+        id: 3,
+        pathway: 'titreTroisiemeProjet',
+        title: 'Titre du troisième projet',
+        picture: '/assets/imgTemplate/1000x750.png',
+        description: 'Longue description du troisième projet, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisi vel consectetur interdum, nisl nunc consectetur nisi, eget consectetur nisi nisl eget consectetur nisi.',
+        projectImages: [
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/1000x750.png',
+        ]
+    },
+    {
+        id: 4,
+        pathway: 'titreQuatriemeProjet',
+        title: 'Titre du quatrième projet',
+        picture: '/assets/imgTemplate/1000x750.png',
+        description: 'Longue description du quatrième projet, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisi vel consectetur interdum, nisl nunc consectetur nisi, eget consectetur nisi nisl eget consectetur nisi.',
+        projectImages: [
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/1000x750.png',
+            '/assets/imgTemplate/370x490.png',
+        ]
+    },
+]
+
+export default projectData;
