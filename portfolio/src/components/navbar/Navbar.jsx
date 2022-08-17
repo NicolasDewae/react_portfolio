57151bba0595bf630f9f75575e2f3b4affd663d5
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2022-08-17 16:19:30 +0200

    blog button put in comment

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
                         <li><a href="/work">Work</a></li>
                         <li><a href="/about">About</a></li>
                         <li><a href="/contact">Contact</a></li>
-                        <li><a href="/blog">Blog</a></li>
+                        {/* <li><a href="/blog">Blog</a></li> */}
                     </ul>
                     <button className="burger" onClick={handleShowLinks}>
                         <span className="burger_line"></span>
