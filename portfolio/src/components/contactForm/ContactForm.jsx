94f8f732b4f4486ead4ae915e0caf99371cb8619
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-08-22 16:38:43 +0200

    add formsubmit.co for send email

--- portfolio/src/components/contactForm/ContactForm.jsx
+++ portfolio/src/components/contactForm/ContactForm.jsx
@@ -2,21 +2,26 @@ import React from "react";
 import "./ContactForm.css";
 
 const ContactForm = () => {
+
     return (
         <>
-            <form className="contact-form" action="">
+            <form className="contact-form" action={`https://formsubmit.co/${process.env.REACT_APP_MY_EMAIL}`} method="POST">
+                <input type="hidden" name="_next" value="/contact"/>
                 <div>
-                    <input type="text" placeholder="Firstname"/>
-                    <input type="text" placeholder="Lastname"/>
+                    <input type="text" name="firstname" placeholder="Firstname"/>
+                    <input type="text" name="lastname" placeholder="Lastname"/>
                 </div>
                 <div>
-                    <input type="email" placeholder="Email"/>
+                    <input type="email" name="email" placeholder="Email Address"/>
                 </div>
                 <div>
-                    <textarea type="text" placeholder="Message"/>
+                    <textarea type="text" name="message" placeholder="Message"/>
                 </div>
                 <div>
-                    <button>Send</button>
+                    <input type="hidden" name="_captcha" value="true"/>
+                </div>
+                <div>
+                    <button type="submit">Send</button>
                 </div>
             </form>
         </>
