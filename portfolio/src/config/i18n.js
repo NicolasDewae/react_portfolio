10405a25310694a1107f72de6e6c48d2d3616cee
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2022-09-29 17:32:46 +0200

    data file for translation

---
+++ portfolio/src/config/i18n.js
@@ -0,0 +1,65 @@
+export const navLink = [
+    {
+        fr: [
+                {
+                    id: 1,
+                    title: 'Accueil',
+                    url: '/',
+                },
+                {
+                    id: 2,
+                    title: 'Projets',
+                    url: '/about',
+                },
+                {
+                    id: 3,
+                    title: 'A propos',
+                    url: '/about',
+                },
+                {
+                    id: 4,
+                    title: 'Contact',
+                    url: '/contact',
+                }
+        ],
+        en: [
+            {
+                id: 1,
+                title: 'Home',
+                url: '/',
+            },
+            {
+                id: 2,
+                title: 'Works',
+                url: '/works',
+            },
+            {
+                id: 3,
+                title: 'About',
+                url: '/about',
+            },
+            {
+                id: 4,
+                title: 'Contact',
+                url: '/contact',
+            }
+        ],
+    }
+]
+
+export const mainContent = [
+    {
+        fr: [
+                {
+                    id: 1,
+                    message: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+                }
+        ],
+        en: [
+            {
+                id: 1,
+                message: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+            }
+        ],
+    }
+]
