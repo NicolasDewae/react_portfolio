761a83036348eb02ab833cdb83c63ba8f6870c77
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2022-12-06 14:20:15 +0100

    article exemple added

--- portfolio/src/config/articles/monPremierArticle.js
+++ portfolio/src/config/articles/monPremierArticle.js
@@ -2,14 +2,14 @@ const monPremierArticle = {
       id: 1,
       pathway: 'titrePremierArticle',
       title: 'Titre du premier article',
-      picture: '/assets/img/street_view-2.jpg',
+      picture: '/assets/img/street/street-2.jpg',
       description: 'Longue description du premier article, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisi vel consectetur interdum, nisl nunc consectetur nisi, eget consectetur nisi nisl eget consectetur nisi.',
       blogImages: [
-          '/assets/img/street_view-2.jpg',
-          '/assets/img/street_view-2.jpg',
-          '/assets/img/street_view-2.jpg',
-          '/assets/img/street_view-2.jpg',
-          '/assets/img/street_view-2.jpg',
+          '/assets/img/streetview/Streetview-2.jpg',
+          '/assets/img/streetview/Streetview-2.jpg',
+          '/assets/img/streetview/Streetview-2.jpg',
+          '/assets/img/streetview/Streetview-2.jpg',
+          '/assets/img/streetview/Streetview-2.jpg',
       ]    
     }
 
