9ddb145a0932c25d83d5574609c290ac666469a5
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2022-12-06 14:19:11 +0100

    dynamic article

--- portfolio/src/config/blogData.js
+++ portfolio/src/config/blogData.js
@@ -1,4 +1,5 @@
 import monPremierArticle from '../config/articles/monPremierArticle';
+import monDeuxiemeArticle from '../config/articles/monDeuxiemeArticle';
 
 const blogData = [
     {
@@ -14,21 +15,20 @@ const blogData = [
         monPremierArticle.blogImages[3],
         monPremierArticle.blogImages[4],
       ]
-      
     },
     {
-        id: 2,
-        pathway: 'titreDeuxiemeArticle',
-        title: 'Titre du second article',
-        picture: '/assets/img/street_view-2.jpg',
-        description: 'Longue description du second article, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisi vel consectetur interdum, nisl nunc consectetur nisi, eget consectetur nisi nisl eget consectetur nisi.',
-        blogImages: [
-            '/assets/img/street_view-2.jpg',
-            '/assets/img/street_view-2.jpg',
-            '/assets/img/street_view-2.jpg',
-            '/assets/img/street_view-2.jpg',
-            '/assets/img/street_view-2.jpg',
-        ]
+      id: monDeuxiemeArticle.id,
+      pathway: monDeuxiemeArticle.pathway,
+      title: monDeuxiemeArticle.title,
+      picture: monDeuxiemeArticle.picture,
+      description: monDeuxiemeArticle.description,
+      blogImages: [
+        monDeuxiemeArticle.blogImages[0],
+        monDeuxiemeArticle.blogImages[1],
+        monDeuxiemeArticle.blogImages[2],
+        monDeuxiemeArticle.blogImages[3],
+        monDeuxiemeArticle.blogImages[4],
+      ]
     }
 ]
 
