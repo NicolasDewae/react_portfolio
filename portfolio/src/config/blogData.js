8837baeba728747841dfd355e5b8c18979f6cf2c
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-12-19 10:58:00 +0100

    blog data updated

--- portfolio/src/config/blogData.js
+++ portfolio/src/config/blogData.js
@@ -1,19 +1,19 @@
-import monPremierArticle from '../config/articles/monPremierArticle';
+import her from './articles/her';
 import monDeuxiemeArticle from '../config/articles/monDeuxiemeArticle';
 
 const blogData = [
     {
-      id: monPremierArticle.id,
-      pathway: monPremierArticle.pathway,
-      title: monPremierArticle.title,
-      picture: monPremierArticle.picture,
-      description: monPremierArticle.description,
+      id: her.id,
+      pathway: her.pathway,
+      title: her.title1,
+      picture: her.picture,
+      description: her.description,
       blogImages: [
-        monPremierArticle.blogImages[0],
-        monPremierArticle.blogImages[1],
-        monPremierArticle.blogImages[2],
-        monPremierArticle.blogImages[3],
-        monPremierArticle.blogImages[4],
+        her.blogImages[0],
+        her.blogImages[1],
+        her.blogImages[2],
+        her.blogImages[3],
+        her.blogImages[4],
       ]
     },
     {
