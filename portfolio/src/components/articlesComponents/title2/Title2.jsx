0112892a7cf79c0af1cca8cfa12ea1a853ce899e
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-12-19 10:57:31 +0100

    H2 component

---
+++ portfolio/src/components/articlesComponents/title2/Title2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Title2.css";
+
+const Title = ({ title }) => {
+    return (
+        <>
+            <div className="title">
+                <h2>{title}</h2>
+            </div>
+        </>
+    );
+}
+
+export default Title;
