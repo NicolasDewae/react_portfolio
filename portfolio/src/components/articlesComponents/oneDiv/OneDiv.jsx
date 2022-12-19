3041b946177a3c00c8f16210bffd47b2e355349a
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-12-19 10:57:05 +0100

    condition img removed

--- portfolio/src/components/articlesComponents/oneDiv/OneDiv.jsx
+++ portfolio/src/components/articlesComponents/oneDiv/OneDiv.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./OneDiv.css";
 
-const OneDiv = ({ data, type }) => {
+const OneDiv = ({ data }) => {
+
     return (
         <>
             <div className="one-div">
-                {/* if data = img */}
-                {type === "img" && <img src={ data } alt="photo de l'article" />}
-                {/* if data = text */}
-                {type === "txt" && <p>{ data }</p>}
+                <p>{ data }</p>
             </div>
         </>
     );
