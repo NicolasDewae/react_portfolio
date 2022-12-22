e0e6a307879958522fd18031a5a7bf6ca0b43650
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-12-22 16:49:29 +0100

    json added

---
+++ portfolio/src/data/blogPageData.js
@@ -0,0 +1,23 @@
+import Her from './articles/her';
+import Drive from './articles/drive';
+
+const blogPageData = [
+    {
+      id: Her.id,
+      pathway: Her.pathway,
+      category: Her.category,
+      title: Her.title.title1,
+      picture: Her.picture.img1,
+      description: Her.description,
+    },
+    {
+      id: Drive.id,
+      pathway: Drive.pathway,
+      category: Drive.category,
+      title: Drive.title.title1,
+      picture: Drive.picture,
+      description: Drive.description,
+    }
+]
+
+export default blogPageData;
