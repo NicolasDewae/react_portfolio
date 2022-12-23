7dc2ed78077358af584d2ac0863c171d99c233be
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-12-23 15:42:51 +0100

    txt correction

--- portfolio/src/data/articles/drive.js
+++ portfolio/src/data/articles/drive.js
@@ -9,7 +9,7 @@ const Drive = {
     title: {
         title1: 'Drive',
         title2: {
-            Introduction: 'Introduction',
+            introduction: 'Introduction',
             regardFuyant: 'Le regard fuyant, mais pas que',
             couleurs: 'Le choix des couleurs',
             contreJour: 'Contre-jour',
@@ -20,7 +20,7 @@ const Drive = {
         },
     },
     paragraphe:{
-        p1: 'J\'ai depuis longtemps envie de vous parler de cinéma et de comment ça m\'inspire en photographie. Donc aujourd\'hui nous allons parler du film « Drive » de Nicolas Winding Refn avec notamment Ryan Gosling, Bryan Cranston (« je suis l\'homme qui frappe à la porte ») et Carey Mulligan qui dans le film a le nom de ma grand-mère.',
+        p1: 'J\'ai depuis longtemps envie de vous parler de cinéma et de comment ça m\'inspire en photographie. Donc aujourd\'hui nous allons parler du film de Nicolas Winding Refn, « Drive ». Avec en autre au casting, Ryan Gosling, Bryan Cranston (« je suis l\'homme qui frappe à la porte ») et Carey Mulligan qui dans le film a le nom de ma grand-mère.',
         p2: 'Ici nous voyons donc Shannon, interprété par Bryan Cranston. Il est à la droite de l\'image, le regard dans le vague partant vers la gauche, il est dans des tons froids et si l\'on suit son regard on arrive vers l\'encadrement d\'une porte d\'où sort une lumière aux tons chauds, faisant contraste avec Shannon.',
         p3: 'Vous l\'aurez donc deviné, c\'est de là que décide d\'arriver Ryan Gosling.',
         p4: [
@@ -29,30 +29,30 @@ const Drive = {
         ],
         p5: [
             'Cette règle est répétée même dans le plan suivant où le point est fait sur Irène et que l\'on voit Ryan dans un miroir. Elle est devant un mur rouge est le chauffeur est entouré d\'un cadre vert.',
-            'Si vous lisez le synopsis proposé par Netflix, il est écrit « La charmante voisine d\'un cascadeur et chauffeur de bande organisée le tire de son isolement… ». Les couleurs de leur première rencontre reflètent bien cette idée, une femme chaleureuse et un homme froids, sans réel attache.',
-            'Le seul moment où cette règle n\'est plus de mise c\'est lorsque Ryan qui la pièce. La « charmante voisine » s\'assoie dos au mur vert.'
+            'Si vous lisez le synopsis proposé par Netflix, il est écrit « La charmante voisine d\'un cascadeur et chauffeur de bande organisée le tire de son isolement… ». Les couleurs de leur première rencontre reflètent bien cette idée, une femme chaleureuse et un homme froids, sans réel attache.',
+            'Le seul moment où cette règle n\'est plus de mise c\'est lorsque Ryan quitte la pièce. La « charmante voisine » s\'assoie dos au mur vert.'
         ],
         p6: [
-            'Ceci laissant paraître une par obscur chez elle ? Je ne sais pas vraiment, mais si on lit la fin du synopsis, on peut y lire « …jusqu\'à ce que son mari violent sorte de prison ».',
+            'Ceci laissant paraître une par obscur chez elle ? Je ne sais pas vraiment, mais si on lit la fin du synopsis, on peut y lire « …jusqu\'à ce que son mari violent sorte de prison ».',
             'Vous remarquerez également sur les photos plus haut, que chacun occupe une place différente dans l\'image, c\'est une pratique courante de champ, contre champ, qui permet de rendre visuel le face à face des deux protagonistes. Sébastien Roignant en parle dans une de ses vidéos, il l\'utilise lors des mariages pour photographier les mariés.',
             'Voici une autre scène un peu plus tard où l\'on retrouve ce code couleur :'
         ],
         p7: [
-            'En photo lorsque l\'on réalise des contre-jour sans flash ni réflecteur ni diffuseur, on expose pour le modèle, ce qui surexpose le fond. En vidéo il est souvent conseillé d\'exposer pour la partie la plus lumineuse, quitte à voir notre modèle en ombre chinoise (bien sûr, si vous faites un plan genre interview, ne laissez pas la personne dans le noir). Je ne suis pas vidéaste, mais il apparait que la surexposition est moins tolérée en vidéo.',
+            'En photo, lorsque l\'on réalise des contre-jour sans flash ni réflecteur ni diffuseur, on expose pour le modèle, ce qui surexpose le fond. En vidéo il est souvent conseillé d\'exposer pour la partie la plus lumineuse, quitte à voir notre modèle en ombre chinoise (bien sûr, si vous faites un plan genre interview, ne laissez pas la personne dans le noir). Je ne suis pas vidéaste, mais il apparait que la surexposition est moins tolérée en vidéo.',
             'Dans l\'image qui suit, nous avons affaire à ce cas'
         ],
         p8: 'Des lignes bien droites, une symétrie parfaite cassée par le personnage. Notez le spot lumineux pour sortir Ryan du noir, la couleur est dans les tons chauds, devinez quelle personne il regarde par la fenêtre…',
         p9: [
             'Pour exprimer le point qui va suivre, je suis obligé de dévoiler des points importants du film.',
             'Le code couleur cité plus haut est bousculé à un moment clef du film. Irène vient de perdre son mari, elle espère malgré tout pouvoir fuir avec le chauffeur. Alors qu\'ils quittent leur immeuble en ascenseur, ils se retrouvent avec un homme, qui est visiblement venu le tuer, sans suit un duel très violent où Ryan sort vainqueur. Nos deux personnages comprennent à cet instant qu\'ils ne pourront pas fuir ensemble. Irène sort de l\'ascenseur, se retrouvant seule dans un parking froid, laissant Ryan dans la chaleur de l\'ascenseur. Les couleurs se sont inversées',
-            'Nous avons droits ici à une scène sans parole, mais avec un jeu d\'acteur très causante'
+            'Nous avons droits ici à une scène sans parole, mais avec un jeu d\'acteur très causant'
         ],
         p10: [
             'Petit plan bonus qui n\'a rien à voir avec la colorimétrie',
             'Voyez ce plan, je l\'affectionne tout particulièrement. Je prends régulièrement des photos du trottoir dans face pour réaliser cette vue, j\'en présente d\'ailleurs une dans mon article : « Mes débuts en photographie de rue ». http://nicolasdwphotographe.com/debuts-photographie-de-rue/',
-            'Ce type de photo me rappelle un jeu auquel je jouais enfant, « Steets of rage »',
-            'En vrai, j\'aimerais beaucoup voir un court métrage réalisé entièrement avec ce plan là'
-        ]
+            'Ce type de photo me rappelle un jeu auquel je jouais enfant, « Steets of rage »',
+        ],
+        p11: 'En vrai, j\'aimerais beaucoup voir un court métrage réalisé entièrement avec ce plan là'
     },
     picture: {
         img1: {
@@ -96,7 +96,7 @@ const Drive = {
             alt: 'lignes expliquant la coupure entre les couleurs chaudes et froides',
         },
         img11: {
-            src: '/assets/imgArticles/drive/ligne_fuite_temperature.jpg',
+            src: '/assets/imgArticles/drive/ligne_fuite_temperature_couleur.jpg',
             alt: 'Bryan Cranston dans son garage avec les lignes de fuite déssinées',
         },
         img12: {
