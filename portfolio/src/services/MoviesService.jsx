dc55cf19c6afaba61dcab0fd776ad876286e0509
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-12-26 17:02:31 +0100

    movie api service

---
+++ portfolio/src/services/MoviesService.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import axios from "axios";
+import { API_BASE_URL, API_KEY } from "../data/globalVar";
+
+export default class MovieService extends React.Component {
+
+    /**
+     * Get movies by id
+     * @param {string} id
+     */
+    getMoviesById(id, setMovies) {
+        axios.get(API_BASE_URL + "/movie/" + id + "?language=fr-FR",{
+            headers: {
+                Authorization: "Bearer " + API_KEY ,
+            },
+        })
+            .then(response => {
+                setMovies(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+}
+    
