64e2c4fc5c59bf09d392eee775cd8a9d8bc2f01f
Nicolas De waegenaere <de_waegenaere@live.fr>
2022-12-26 17:01:38 +0100

    global var

---
+++ portfolio/src/data/globalVar.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL = 'https://api.themoviedb.org/3';
+export const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkM2YwNTBlNjRiODkyZTUwY2EwOTJmMTc2ZmExOWJlNyIsInN1YiI6IjYzYTk1ZjMwMTVhNGExMDA4YTIxMDllOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._4PkMhMQUVGvJGVtFj2LY1bYqtWya3fpXW_TnBQM1A0';
