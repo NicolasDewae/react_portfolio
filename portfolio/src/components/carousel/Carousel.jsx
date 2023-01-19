298a77ddddafc5e1b9e3cb692731e918f9106b78
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2023-01-19 13:46:56 +0100

    js caroussel

--- portfolio/src/components/carousel/Carousel.jsx
+++ portfolio/src/components/carousel/Carousel.jsx
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
 import './Carousel.css';
 
 const Carousel = ({ picture1, picture2, picture3, picture4 }) => {
-    
-        return (
-            <>
-                <div className="slider">
-                    <div className="slides">
-                        <div className="slide">
-                            <img src={picture1} alt="photo 1" />
-                        </div>
-                        <div className="slide">
-                            <img src={picture2} alt="photo 2" />
-                        </div>
-                        <div className="slide">
-                            <img src={picture3} alt="photo 3" />
-                        </div>
-                        <div className="slide">
-                            <img src={picture4} alt="photo 4" />
-                        </div>        
-                    </div>
-                </div>
-            </>
-        );
-    }
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const images = [
+        { url: picture1 },
+        { url: picture2 },
+        { url: picture3 },
+        { url: picture4 }
+    ];
+
+    // Go to the previous image
+    const handlePrevious = () => {
+        setCurrentIndex((currentIndex + images.length - 1) % images.length);
+    };
+
+    // Go to the next image
+    const handleNext = () => {
+        setCurrentIndex((currentIndex + 1) % images.length);
+    };
+
+    return (
+        <div className="carousel-container">
+        <div className="arrow previous" onClick={handlePrevious}>
+            <FontAwesomeIcon icon={faChevronLeft}/>
+        </div>
+        <img src={images[currentIndex].url} alt="carousel" className="carousel-image" />
+        <div className="arrow next" onClick={handleNext}>
+        <FontAwesomeIcon icon={faChevronRight} />
+        </div>
+        </div>
+    );
+};
 
 export default Carousel;
