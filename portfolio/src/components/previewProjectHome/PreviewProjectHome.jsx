b9265b9b625cb33b7553e7d496f85b8c3f04d48d
Nicolas De waegenaere <de_waegenaere@live.fr>
2023-03-28 16:01:33 +0200

    new toggle section for homepage

--- portfolio/src/components/previewProjectHome/PreviewProjectHome.jsx
+++ portfolio/src/components/previewProjectHome/PreviewProjectHome.jsx
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PreviewProjectHome.css';
 import { Link } from 'react-router-dom';
 import Split from '../split/Split';
 
-const PreviewProjectHome = ({
-    projects: {
-        picture,
-        title,
-        pathway,
-    },
-    data
-}) => {
+const PreviewProjectHome = ({ projects, data }) => {
     
+    const [previewClass, setClassName] = useState('preview-container');
+    const [iconClass, setIconClass] = useState('plus');
+    const [projectButton, setProjectButton] = useState('projectButton-flex');
+
+    const changeClass = () => {
+        setClassName(previewClass === 'preview-container' ? 'preview-container-hover' : 'preview-container');
+        setIconClass(iconClass === 'plus' ? 'minus' : 'plus');
+        setProjectButton(projectButton === 'projectButton-flex' ? 'projectButton-block' : 'projectButton-flex');
+    };
+
     return (
         <>
-        <div className='preview-container'>
-
-            <div className="preview-containt">
-                <div className="picture">
-                    <img src={picture} alt="photo du projet" />
+            <section className={previewClass} onClick={changeClass}>
+                {previewClass === 'preview-container-hover' &&
+                    <div className="preview-containt">
+                        <div className="picture">
+                            <img src={projects.picture} alt="photo du projet" />
+                        </div>
+                    </div>
+                }
+                <div className={projectButton}>
+                    <div className="title">
+                        <h2>{projects.title}</h2>
+                    </div>
+                    <Link to={`/project/${projects.pathway}`}>
+                        <a className="button">
+                            {data[0].button}
+                        </a>
+                    </Link>
                 </div>
-            </div>
-            <div className='projectButton'>
-                <div className="title">
-                    <h2>{title}</h2>
+                <div className={iconClass}>
+                    <p>+</p>
                 </div>
-                <Link to={`/project/${pathway}`}>
-                    <a className="button">
-                        {data[0].button}
-                    </a>
-                </Link>
-            </div>
-        </div>
+            </section>
             <div>
                 <Split />
             </div>
